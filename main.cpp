5dd33ffce085342bf4b64a08c9f02cf602d2d30a
IrouKaizen <122926735+IrouKaizen@users.noreply.github.com>
2023-03-17 08:47:53 +0000

    Programme_température_celsius_fahrenheit
    Ce_code_en_C++_demande_à_l_utilisateur_de_saisir_une_température_de_début_une_température_de_fin_et_un_pas_de_progression_puis_affiche_un_tableau_de_conversion_des_températures_de_Celsius_à_Fahrenheit_avec_une_précision_définie_par_le_pas_de_progression.

--- main.cpp
+++ main.cpp
@@ -1,38 +1,47 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    float n1, n2, re;
-    char operateur;
+    double tempDebut, tempFin, pas;
 
-    cout << "Programme calculatrice arithmÃ©tique" << endl;
-    cout <<"Entrez le premier nombre : " << endl;
-    cin >> n1;
-    cout <<"Entrez le second nombre : " << endl;
-    cin >> n2;
-    cout <<"PrÃ©cisez l'opÃ©rateur : " << endl;
-    cin >> operateur;
+    ///Ce code en C++ demande à l'utilisateur de saisir une température de début, une température de fin et un pas de progression,
+    ///puis affiche un tableau de conversion des températures de Celsius à Fahrenheit avec une précision définie par le pas de progression.
 
-    if(operateur == '+')
+    // Demande la température de début
+    cout << "Entrez la temperature de debut : ";
+    cin >> tempDebut;
+
+    // Demande la température de fin
+    cout << "Entrez la temperature de fin : ";
+    cin >> tempFin;
+
+    // Demande le pas de progression
+    cout << "Entrez le pas de progression : ";
+    cin >> pas;
+
+    // Vérifie les cas impossibles
+    if (tempFin < tempDebut)
     {
-        re = n1 + n2;
-    }
-    else if(operateur == '-')
-    {
-        re = n1 - n2;
-    }
-    else if(operateur == '/')
-    {
-        re = n1 / n2;
-    }
-    else if(operateur == '*')
-    {
-        re = n1 * n2;
+        cout << "Erreur : la temperature de fin ne peut pas etre inferieure a la temperature de debut." << endl;
+        return 1;
     }
 
-    cout <<"Voici le resultat : ", re << endl;
+    if (pas <= 0 || pas > (tempFin - tempDebut))
+    {
+        cout << "Erreur : le pas de progression doit etre strictement positif et inferieur a l'intervalle de temperatures." << endl;
+        return 1;
+    }
+
+    // Affiche le tableau
+    cout << setw(15) << left << "Temperature" << setw(15) << left << "Fahrenheit" << setw(15) << left << "Celsius" << endl;
+
+    for (double t = tempDebut; t <= tempFin; t += pas)
+    {
+        double f = 9.0 / 5.0 * t + 32.0;
+        cout << setw(15) << left << t << setw(15) << left << f << setw(15) << left << (f - 32.0) * 5.0 / 9.0 << endl;
+    }
 
     return 0;
 }
